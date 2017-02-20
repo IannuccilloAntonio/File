8f0d0d86f5a6a37eba107b0619406e198467b3e7
alunno <alunno@lab03-06>
2017-02-20 12:06:08 +0100

    da completare

---
+++ punto.py
@@ -0,0 +1,14 @@
+class Punto:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.p = Punto (10)
+    def set_punto_x(self, x):
+        self.x = x
+    def get_punto_x(self):
+        return self.x
+    def set_punto_y(self, y):
+        self.y = y
+    def get_punto_y(self):
+        return self.y
+     
