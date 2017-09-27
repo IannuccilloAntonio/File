ec37402881157160b8f62798d0b9564e84038a81
Antonio Iannuccillo <iannuccillo.antonio99@gmail.com>
2017-09-27 12:53:53 +0200

    Add files via upload

---
+++ ciao.php
@@ -0,0 +1,52 @@
+<html>
+
+<head>
+	<title>Benvenuto</title>
+</head>
+
+<body>
+
+
+
+<div style = "Width:100% ; height:30% ; float:left ; background-color:#c0c0c0 ; color:red ; border:5px solid #2f4f4f; border-radius: 20px"><font size= "150"><center><h1><b><em>IL MATTINO</em></b></h1></center></font></div>
+
+<div style = "Width:20% ; height:7% ; float:left ; background-color:green ; color:black ; border-radius:7px; text-align:center;margin-top:3px "><h3>HOME PAGE</h3></div>
+
+<div style = "Width:20% ; height:7% ; float:left ; background-color:yellow ; color:black ; border-radius:7px; text-align:center;margin-top:3px"><h3>CONTATTI</h3></div>
+
+<div style = "Width:20% ; height:7% ; float:left ; background-color:red ; color:black ; border-radius:7px; text-align:center; margin-top:3px"><h3>PAGINE</h3></div>
+
+<div style = "Width:20% ; height:7% ; float:left ; background-color:blue ; color:black ; border-radius:7px; text-align:center; margin-top:3px"><h3>COLLEGAMENTI</h3></div>
+
+<div style = "Width:20% ; height:7% ; float:left ; background-color:orange ; color:black ; border-radius:7px; text-align:center ; margin-top:3px"><h3>CIAO</h3></div>
+
+<div style = "Width:17% ; height:100% ; float:left ; background-color:#00ffff ; color:black ;padding-left:10px ; padding-right:10px ; border:2px  solid #FF0000; margin-top:3px; padding-top:5px; font-size:30 ">
+
+<em>EDITORIALE</em>
+<div style="Width:15% ; height:63% ; float:left ; background-color:#00ffff ; color:black ;padding-left:10px ; padding-right:10px  ; font-size : 20; position:absolute"><b>Da oggi la giustizia
+tratta tutti da mafiosi</b></div>
+<div style="Width:15% ; height:63% ; float:left ; background-color:#00ffff ; color:black ;padding-left:10px ; padding-right:10px; margin-top:51px  ; font-size : 15; position:absolute"><span style=" font-size: 23; font-family: fantasy">C</span>ari lettori , mentre leggete questo articolo il Parlamento sta votando perché la giustizia del sospetto diventi in Italia non non una patologia del sistema, non un'eccezione, ma la regola.</div>
+</div>
+
+
+<div style = "Width:79% ; height:100% ; float:left ; background-color:#a52a2a ; color:#00ffff ; padding-left:10px ; padding-right: 10px ;border:2px  solid #c0c0c0; margin-top:3px; padding-top:3px">
+<em>NOTIZIE DEL GIORNO</em>
+<div style = "Width:70% ; height:58% ; float:left ; background-color:#a52a2a ; color:black ; padding-left:10px ; padding-right: 10px ; margin-top:10px; position:absolute; font-size:25;font-femily:fantasy ; ">
+<img border-radius:7px 7px 7px 7px height=30% width=25% src="http://www.ilmattino.it/photos/LOW/48/25/3264825_pckg_120150646759880206-20170927.jpg.pagespeed.ce.aXHg70MV-v.jpg">
+<br>
+<b>Statali, piu' soldi:<br>
+salvati gli 80 euro</b>
+<br><span style="font-size:14; padding-top:4px; color:grey"><em>di Andrea Bassi</em></span>
+</div>
+
+<?php  
+for ($t=0;$t<5;$t++) {
+<br>echo $t;
+
+}
+ ?>
+</div>
+</body>
+
+
+</html>
